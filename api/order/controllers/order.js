dbcf1049ee3280a35557e8ff4db1d6d4c7d2957f
marcioandrade <marcioandrade.dev@gmail.com>
2022-01-07 23:24:00 -0300

    Buscando dados do carrinho no Backend

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -5,8 +5,24 @@
  * to customize this controller
  */
 
-module.exports = {
-  createPaymentIntent: () => {
-    return 'Hello World!'
-  }
+ module.exports = {
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+
+    let games = [];
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id,
+        });
+
+        if (validatedGame) {
+          games.push(validatedGame);
+        }
+      })
+    );
+
+    return games;
+  },
 };
